c3e68f81753ee8e8a79b68d4105d47b02cc42e36
Andrei Zhigalkin <sanks64@protonmail.com>
2023-02-07 19:29:56 +0600

    fish: Set $DOCKER_CONFIG to $XDG_CONFIG_HOME/docker

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,8 @@ if status is-interactive
     set -x XDG_DATA_HOME $HOME/.local/share
     set -x XDG_STATE_HOME $HOME/.local/state
 
+    set -x DOCKER_CONFIG $XDG_CONFIG_HOME/docker
+
     fish_add_path $HOME/.dotfiles/.local/bin
     fish_add_path $HOME/.local/bin
 
